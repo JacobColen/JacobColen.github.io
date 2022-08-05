e4745642bb468a50d02ec7e8727d8c10c57424b2
JacobColen <84134905+JacobColen@users.noreply.github.com>
2022-08-05 13:31:30 +0200

    Strechtmode set to true

--- games/TestGame/js/plugins/DRGV.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var Graphics__centerElement = Graphics._centerElement;
-  Graphics._centerElement = function() {
-    Graphics__centerElement.call(this);
-
-    this._canvas.style.width = "100%";
-    this._canvas.style.height = "100%";
-  }
-})();
