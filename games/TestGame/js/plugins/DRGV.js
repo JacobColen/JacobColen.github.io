b8f62f68197181684d6ce67e68ac30cfd1479390
JacobColen <84134905+JacobColen@users.noreply.github.com>
2022-08-05 13:25:32 +0200

    Update DRGV.js

--- games/TestGame/js/plugins/DRGV.js
+++ games/TestGame/js/plugins/DRGV.js
@@ -1,3 +1,9 @@
-var gameCanvas = document.getElementById("GameCanvas");
-gameCanvas.style["width"] = "100%";
-gameCanvas.style["height"] = "100%";
+(function() {
+  var Graphics__centerElement = Graphics._centerElement;
+  Graphics._centerElement = function() {
+    Graphics__centerElement.call(this);
+
+    this._canvas.style.width = "100%";
+    this._canvas.style.height = "100%";
+  }
+})();
